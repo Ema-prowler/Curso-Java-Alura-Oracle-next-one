92db31538ddab543b5aa5800c5db167ddb28ed0e
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-06-11 18:00:07 -0300

    creando otra clase y utilizando condicionales if, else

--- primerprograma/src/Main.java
+++ primerprograma/src/Main.java
@@ -18,7 +18,8 @@ public class Main {
                 """ + fechaDeLanzamiento;
         System.out.println(sinopsis);
 
-
+        int clasificacion = (int)(media / 2);
+        System.out.println(clasificacion);
 
     }
 }

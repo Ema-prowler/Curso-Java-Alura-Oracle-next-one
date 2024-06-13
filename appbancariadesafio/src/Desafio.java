afbab68c2b07bb844bb3991ab858f991ed02d5ce
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-06-13 14:11:49 -0300

    Creando todos los metodos del menu

--- appbancariadesafio/src/Desafio.java
+++ appbancariadesafio/src/Desafio.java
@@ -6,13 +6,13 @@ public class Desafio {
 
         String nombre = "Tony Stark";
         String tipoDeCuenta = "Corriente";
-        double Saldo = 1599.99;
+        double saldo = 1599.99;
         int opcion = 0;
 
         System.out.println("*********************************");
         System.out.println("\nNombre del cliente: " + nombre);
         System.out.println("El tipo de cuenta es: " + tipoDeCuenta);
-        System.out.println("El saldo disponible es: " + Saldo + "$");
+        System.out.println("El saldo disponible es: " + saldo + "$");
         System.out.println("\n*********************************");
 
         String menu = """
@@ -26,7 +26,32 @@ public class Desafio {
         while (opcion != 9) {
             System.out.println(menu);
             opcion = teclado.nextInt();
-
+            switch (opcion) {
+                case 1:
+                    System.out.println("El saldo actualizado es: " + saldo + "$");
+                    break;
+                case 2:
+                    System.out.println("Cual es el monto que desea retirar?");
+                    double valorAretirar = teclado.nextDouble();
+                    if (valorAretirar > saldo) {
+                        System.out.println("Saldo insuficiente");
+                    }else {
+                        saldo = saldo - valorAretirar;
+                        System.out.println("Saldo actualizado: " + saldo + "$");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Cual es el monto que desea depositar?");
+                    double valorDepositar = teclado.nextDouble();
+                    saldo = saldo + valorDepositar;
+                    System.out.println("Saldo actualizado: " + saldo + "$");
+                    break;
+                case 9:
+                    System.out.println("El programa a terminado");
+                    break;
+                default:
+                    System.out.println("Opcion invalida Seleccione una opcion valida (1-2-3-9)");
+            }
         }
     }
 }

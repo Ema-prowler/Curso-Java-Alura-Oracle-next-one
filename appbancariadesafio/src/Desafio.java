e0477a3c758d2d0493ac206021552c0302f5bd27
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-06-13 13:45:29 -0300

    Adicionando datos del cliente/usuiario

---
+++ appbancariadesafio/src/Desafio.java
@@ -0,0 +1,15 @@
+public class Desafio {
+
+    public static void main(String[] args) {
+
+        String nombre = "Tony Stark";
+        String tipoDeCuenta = "Corriente";
+        double Saldo = 1599.99;
+
+        System.out.println("***********************");
+        System.out.println("\nNombre del cliente: " + nombre);
+        System.out.println("El tipo de cuenta es: " + tipoDeCuenta);
+        System.out.println("El saldo disponible es: " + Saldo + "$");
+        
+    }
+}
